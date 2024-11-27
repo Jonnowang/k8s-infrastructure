c7057fc3584d6d038d2b085e300d88dc4a49c82f
Jonathan <jonnowangnz@gmail.com>
2024-11-27 20:52:07 +0000

    clean terraform and add kubernetes yaml

--- terraform/tf-compartment/compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "tf-compartment" {
-    # Required
-    compartment_id = "ocid1.tenancy.oc1..aaaaaaaa6pn45lyesehihf3dafl6c2p46ggpdutmmvnm6nvwmnhwwqdp2vgq"
-    description = "Basic compartment for Terraform resources."
-    name = "constellation-simple"
-}
--- terraform/tf-oke-cluster/public-security-list.tf
+++ terraform/tf-oke-cluster/public-security-list.tf
@@ -39,7 +39,7 @@ resource "oci_core_security_list" "public-security-list"{
       }
   }
 
-  # Kubernetes API ingress
+  # Kubernetes Control Plane API ingress
   ingress_security_rules {
       stateless   = false
       source      = "0.0.0.0/0"
@@ -52,30 +52,30 @@ resource "oci_core_security_list" "public-security-list"{
   }
 
   ingress_security_rules { 
-      stateless = false
-      source = "0.0.0.0/0"
-      source_type = "CIDR_BLOCK"
-      # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
-      protocol = "1"
-  
-      # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
-      icmp_options {
-        type = 3
-        code = 4
-      } 
-    }   
-  
+    stateless = false
+    source = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+      code = 4
+    } 
+  }   
+
   ingress_security_rules { 
-      stateless = false
-      source = "10.0.0.0/16"
-      source_type = "CIDR_BLOCK"
-      # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
-      protocol = "1"
-  
-      # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
-      icmp_options {
-        type = 3
-      } 
-    }
+    stateless = false
+    source = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+    } 
+  }
 }
   
